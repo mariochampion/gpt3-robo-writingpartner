a1f3d13a021a21b488af86fbab43871870908e2f
mariochampion <mario@mariochampion.com>
2021-01-03 11:23:33 -0600

    just text colors

---
+++ colors.py
@@ -0,0 +1,5 @@
+#######################################
+## ADD SOME COLOR
+# pinched and tweaked from https://github.com/impshum/Multi-Quote/blob/master/run.py
+class color:
+  white, cyan, blue, red, green, yellow, magenta, black, gray, bold = '\033[0m', '\033[96m','\033[94m', '\033[91m','\033[92m','\033[93m','\033[95m', '\033[30m', '\033[30m', "\033[1m"  
